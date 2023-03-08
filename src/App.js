7c9f22c62fd73c80352523ca41b34e9122f0bb64
josue <joalorro@gmail.com>
2023-03-08 15:50:32 -0500

    add button component to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { Button } from './Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         Welcome to Hello World World
+        <Button text="Send Requests"/>
       </header>
     </div>
   );
