8be288b83130f55372d602fea84ded5c1dc805ae
josue <joalorro@gmail.com>
2023-03-08 16:42:00 -0500

    add some functionality to button pressing

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types"
 import { Button as MaterialButton } from "@mui/material"
 
+const defaultHandleClick = (e) => console.log(e)
+
 export const Button = (props) => {
     return  (
-        <MaterialButton variant="contained">
+        <MaterialButton variant="contained" onClick={props.onClick}>
             {props.text}
         </MaterialButton>
     )
--- src/routes/requests/requests.js
+++ src/routes/requests/requests.js
@@ -1,5 +1,7 @@
-import { Link } from "react-router-dom";
-import { Button } from "../../components/Button";
+import { Link } from 'react-router-dom';
+import { Button } from '../../components/Button';
+
+const handleClick = () => console.log('BEEF');
 
 export const Requests = () => {
   return (
@@ -7,7 +9,13 @@ export const Requests = () => {
       <Link to="/">
         <Button text="Back to homepage" />
       </Link>
-      <div>Welcome to the requests page</div>
+      <section>
+        <h1>Welcome to the requests page</h1>
+      </section>
+      <section>
+        <h3>Send GET Request to api/example </h3>
+        <Button text="Send" onClick={handleClick} />
+      </section>
     </div>
   );
 };
