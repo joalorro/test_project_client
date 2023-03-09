797fd29f8267947f22190daf321bf039675de796
josue <joalorro@gmail.com>
2023-03-09 12:42:56 -0500

    added axios, updated requests page to send requests on click

--- src/routes/requests/requests.js
+++ src/routes/requests/requests.js
@@ -1,7 +1,38 @@
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { Button } from '../../components/Button';
 
-const handleClick = () => console.log('BEEF');
+const BASE_URL = 'http://localhost:3001';
+const EXAMPLES_URL = `${BASE_URL}/examples`;
+
+const handleGet = async () => {
+  try {
+    const response = await axios({
+      url: EXAMPLES_URL,
+      method: 'get',
+    });
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const handlePost = async () => {
+  try {
+    const data = [4, 5, 6];
+    const response = await axios({
+      method: 'post',
+      url: EXAMPLES_URL,
+      data,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    console.log('RESPONSE:::', response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const Requests = () => {
   return (
@@ -13,8 +44,12 @@ export const Requests = () => {
         <h1>Welcome to the requests page</h1>
       </section>
       <section>
-        <h3>Send GET Request to api/example </h3>
-        <Button text="Send" onClick={handleClick} />
+        <h3>Send GET Request to /examples </h3>
+        <Button text="Send" onClick={handleGet} />
+      </section>
+      <section>
+        <h3>Send POST Request to /examples </h3>
+        <Button text="Send" onClick={handlePost} />
       </section>
     </div>
   );
