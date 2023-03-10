38572dceb3f60110910c122849f07ec277a37afa
josue <joalorro@gmail.com>
2023-03-10 12:27:49 -0500

    expand post /examples section

--- src/routes/requests/requests.js
+++ src/routes/requests/requests.js
@@ -3,6 +3,8 @@ import { Header } from '../../components/Header/header';
 import { Button } from '../../components/Button';
 
 import './requests.css';
+import { useState } from 'react';
+import { TextField } from '@mui/material';
 
 const BASE_URL = 'http://localhost:3001';
 const EXAMPLES_URL = `${BASE_URL}/examples`;
@@ -19,24 +21,37 @@ const handleGet = async () => {
   }
 };
 
-const handlePost = async () => {
-  try {
-    const data = [4, 5, 6];
-    const response = await axios({
-      method: 'post',
-      url: EXAMPLES_URL,
-      data,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    console.log('RESPONSE:::', response.data);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 export const Requests = () => {
+  const [postRequestField, setPostRequestField] = useState('');
+  const [postResponse, setPostResponse] = useState('');
+
+  const textFieldChange =
+    (handleChangeFn = () => {}) =>
+    (e) => {
+      handleChangeFn(e.target.value);
+    };
+
+  const handlePost =
+    (data, setTextField = () => {}, setResponseData = () => {}) =>
+    async () => {
+      try {
+        const response = await axios({
+          method: 'post',
+          url: EXAMPLES_URL,
+          data,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        setResponseData(response.data);
+      } catch (error) {
+        console.log('ERR');
+        // console.error(error);
+        setTextField('');
+        setResponseData(error.message);
+      }
+    };
+
   return (
     <>
       <Header mountHeader={true} />
@@ -48,10 +63,30 @@ export const Requests = () => {
           <h3>Send GET Request to /examples </h3>
           <Button text="Send" onClick={handleGet} />
         </section>
-        <section>
+        <div className="requests-page__section">
           <h3>Send POST Request to /examples </h3>
-          <Button text="Send" onClick={handlePost} />
-        </section>
+          <TextField
+            variant="standard"
+            onChange={textFieldChange(setPostRequestField)}
+            label="Input"
+            value={postRequestField}
+          />
+          <Button
+            text="Send"
+            onClick={handlePost(
+              postRequestField,
+              setPostRequestField,
+              setPostResponse,
+            )}
+          />
+          <TextField
+            variant="outlined"
+            disabled={true}
+            value={postResponse}
+            label="API Response:"
+            multiline={true}
+          />
+        </div>
       </div>
     </>
   );
